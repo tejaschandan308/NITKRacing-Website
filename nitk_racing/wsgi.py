e916095c1fdae068132cb321ffd3647aa607e492
sourceganesh <soccerganesh@gmail.com>
2020-09-03 18:49:18 +0530

    wgdi files revert

--- nitk_racing/wsgi.py
+++ nitk_racing/wsgi.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nitk_racing.settings')
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
