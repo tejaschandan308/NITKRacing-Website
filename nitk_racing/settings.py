1c5c51ba88ed91eea484596f069aa8873cb1d8ac
sourceganesh <soccerganesh@gmail.com>
2020-09-03 18:51:53 +0530

    staticdir added

--- nitk_racing/settings.py
+++ nitk_racing/settings.py
@@ -131,10 +131,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'staticfiles'),
-    os.path.join(BASE_DIR,  'nitkracing_ev/static'),
-    # Add to this list all the locations containing your static files 
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
